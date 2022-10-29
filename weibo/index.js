e837b5c70bc14f30f376fa88bf4402731bf94247
XIAOBAIZZH <xiaobai520202103@126.com>
2022-10-29 15:17:25 +0800

    Site updated: 2022-10-29 15:17:25

---
+++ weibo/index.js
@@ -0,0 +1,31 @@
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script>new Vue({
+  el: "#myWeibo", // el不要是最外面的id_name，应该是html: ''里的div的id
+  data: function () {
+    return {
+      content: [],
+      classOption: {
+        singleHeight: 30,
+      },
+    };
+  },
+  created() {
+    this.getWeiboList();
+  },
+  methods: {
+    // 请求开源api, 获取历史上的今天数据
+    getWeiboList() {
+      fetch("https://tenapi.cn/resou/", {
+        method: "GET", // *GET, POST, PUT, DELETE, etc.
+      })
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          this.content = data.list;
+        })
+        .catch((err) => {
+          console.log("err", err);
+        });
+    },
+  },
+});
